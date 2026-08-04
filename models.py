990043464bf872eb8281090f6a965bce97ffac5c
sahdan17 <sahdan.wira.17@gmail.com>
2026-08-04 10:37:55 +0700

    up

--- models.py
+++ models.py
@@ -25,3 +25,27 @@ class Spot(db.Model):
 
     def __repr__(self):
         return f"<Spot {self.spot_name}>"
+
+class PredRes(db.Model):
+    __tablename__ = 'pred_res'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pred_res_id = db.Column(db.String(15), nullable=False, unique=True)
+    tline_id = db.Column(db.String(255), nullable=False)
+    drop_index = db.Column(db.Integer, nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    google_maps_link = db.Column(db.String(255), nullable=True)
+    final_estimate = db.Column(db.Double, nullable=False)
+    estimate_std = db.Column(db.Double, nullable=False)
+    confidence = db.Column(db.String(50), nullable=False)
+    method_estimates = db.Column(db.JSON, nullable=False)
+    method_weights = db.Column(db.JSON, nullable=False)
+    gradients = db.Column(db.JSON, nullable=False)
+    regions = db.Column(db.JSON, nullable=False)
+    hgl_fit = db.Column(db.JSON, nullable=True)
+    sensors = db.Column(db.JSON, nullable=False)
+    is_saved = db.Column(db.Boolean, nullable=False, default=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"<PredRes {self.pred_res_id} - {self.tline_id}>"
